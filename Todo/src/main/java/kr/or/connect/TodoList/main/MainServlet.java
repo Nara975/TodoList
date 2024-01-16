e615bca23acf8385c6e51a8afaf0481337c70e0e
Nara Shin <storia371@gmail.com>
2024-01-16 16:03:19 +0900

    MainServlet

--- Todo/src/main/java/kr/or/connect/TodoList/main/MainServlet.java
+++ Todo/src/main/java/kr/or/connect/TodoList/main/MainServlet.java
@@ -17,7 +17,7 @@ import kr.or.connect.TodoList.dto.TodoDto;
 /**
  * Servlet implementation class MainServlet
  */
-@WebServlet("/MainServlet")
+@WebServlet("/main")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -39,10 +39,22 @@ public class MainServlet extends HttpServlet {
 		
 		request.setAttribute("dtolist", dtolist);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/main.jsp");
-		rd.forward(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+		rd.forward(request, response); 
 		
 	}
+	
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 
 
 
